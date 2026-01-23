bea00a45f77c27a2f773d8c4559154d453a6cebc
yaserrock7896 <yakub.yaseen0901@gmail.com>
2026-01-24 00:32:59 +0530

    yes

--- day 15/task30.js
+++ day 15/task30.js
@@ -3,7 +3,7 @@ let a = `yaser`
 //printing the variable to the console
 console.log(a);
 /*additon of two numbers*/
-let a = 20;
-let b = 30;
-let sum = a + b;
+let b = 20;
+let c = 30;
+let sum = b + c;
 console.log("Sum:", sum);
